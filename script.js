6d98d8486dc2dece49ace14cd453c40cb4e199cd
anasteshi <anasteshi13@gmail.com>
2025-06-08 22:57:46 +0300

    temporary

--- script.js
+++ script.js
@@ -2,12 +2,42 @@ const canvas = document.getElementById("game")
 const context = canvas.getContext("2d")
 const ground = document.getElementById("ground")
 const berry = document.getElementById("berry")
+const headLeft = document.getElementById("head-left")
+const headRight = document.getElementById("head-right")
+const headUp = document.getElementById("head-up")
+const headDown = document.getElementById("head-down")
+const bodyV = document.getElementById("body-vertical")
+const bodyH = document.getElementById("body-horizontal")
+const tailLeft = document.getElementById("tail-left")
+const tailRight = document.getElementById("tail-right")
+const tailUp = document.getElementById("tail-up")
+const tailDown = document.getElementById("tail-down")
 const tile = 74
+
+const head = {
+    left: headLeft,
+    right: headRight,
+    up: headUp,
+    down: headDown
+}
+const body = {
+    left: bodyH,
+    right: bodyH,
+    up: bodyV,
+    down: bodyV
+}
+const tail = {
+    left: tailLeft,
+    right: tailRight,
+    up: tailUp,
+    down: tailDown
+}
 
 let food = {
     x: 10,
     y: 10
 }
+let activeImage
 function placeFood(food) {
     food.x = Math.floor(Math.random()*7)* tile + 10
     food.y = Math.floor(Math.random()*7)* tile + 10
@@ -36,8 +66,17 @@ function drawGame() {
     context.drawImage(ground, 0, 0)
     context.drawImage(berry, food.x, food.y)
     for (let i = 0; i < snake.length; i++) {
-        context.fillStyle = "red"
-        context.fillRect(snake[i].x-6, snake[i].y-6, tile, tile)
+        if (i == 0)
+            context.drawImage(head[direction], snake[i].x-6, snake[i].y-6, tile, tile)
+            // activeImage = head[direction]
+        else {
+            context.fillStyle = "green"
+            context.fillRect(snake[i].x-6, snake[i].y-6, tile, tile)
+        }
+        // else if (i == snake.length - 1)
+        //     activeImage = tail[direction]
+        // else activeImage = body[direction]
+        // context.drawImage(activeImage, snake[i].x-6, snake[i].y-6, tile, tile)
     }
 
     if (snakeX === food.x && snakeY === food.y) {
@@ -65,4 +104,4 @@ function drawGame() {
     snake.unshift(newHead)
 }
 
-let game = setInterval(drawGame, 100) 
+let game = setInterval(drawGame, 120) 
